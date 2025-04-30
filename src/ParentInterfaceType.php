96b0e1ac964ddb1789a37fb47aa25a1f117d2d2b
Rudie Dirkx <github@hotblocks.nl>
2025-04-30 22:55:33 +0200

    ParentInterfaceType name fix

--- src/ParentInterfaceType.php
+++ src/ParentInterfaceType.php
@@ -12,17 +12,11 @@ abstract class ParentInterfaceType extends InterfaceType {
 
 	public function __construct() {
 		$config = [];
-		$config['name'] = $this->name();
 		$config['fields'] = $this->fields();
 		$config['resolveType'] = function($object, GraphQLContext $context, ResolveInfo $info) {
 			return $this->resolveType($object, $context, $info);
 		};
 		parent::__construct($config);
-	}
-
-	public function name() : string {
-		$class = new ReflectionClass($this);
-		return preg_replace('#Interface#', '', $class->getShortName());
 	}
 
 	public function resolveType($objectValue, /*GraphQLContext*/ $context, ResolveInfo $info) {
